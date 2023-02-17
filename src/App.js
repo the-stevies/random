4a1ee695a7b4ae07956c0d20e314013d21603539
stefan <post@paperjazz.eu>
2023-02-17 09:28:25 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in 10 Minutes
         </a>
       </header>
     </div>
